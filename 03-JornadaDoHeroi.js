d3e8f9343dc19d8277bc8954482bf00ce54d890e
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-14 15:48:19 -0300

    Update 03-JornadaDoHeroi.js

--- 03-JornadaDoHeroi.js
+++ 03-JornadaDoHeroi.js
@@ -1,42 +1,34 @@
-// INSTRUCTIONS 
 /*
-[Desafio] Jornada do Herói
+  # [Challenge] Jornada do Herói
+  
+    ## INSTRUCTIONS 
+      Você é um jovem herói que embarca em uma jornada épica para derrotar o temido dragão que aterroriza o reino. 
+      No entanto, você precisa atravessar uma floresta perigosa para chegar à caverna do dragão. 
+      Cada passo é crucial, e sua jornada será determinada pela lógica afiada que você possuir.
 
-## DESCRIÇÃO
-Você é um jovem herói que embarca em uma jornada épica para derrotar o temido dragão que aterroriza o reino. 
-No entanto, você precisa atravessar uma floresta perigosa para chegar à caverna do dragão. 
-Cada passo é crucial, e sua jornada será determinada pela lógica afiada que você possuir.
+    ## OBJECTIVE
+      Escreva um algoritmo que simule a jornada do herói pela floresta. 
+      O herói começa em uma posição inicial e deve dar uma série de passos para atravessar a floresta até a caverna do dragão.
 
-## OBJETIVO
-Escreva um algoritmo que simule a jornada do herói pela floresta. 
-O herói começa em uma posição inicial e deve dar uma série de passos para atravessar a floresta até a caverna do dragão.
+      ### INPUT
+        A posição inicial do herói na floresta (um número inteiro).
+        O número total de passos que o herói deve dar (um número inteiro).
 
-### ENTRADA
-A posição inicial do herói na floresta (um número inteiro).
-O número total de passos que o herói deve dar (um número inteiro).
-
-### SAÍDA
-Imprima a posição final do herói após dar a quantidade de passos especificada.
+      ### OUTPUT
+        Imprima a posição final do herói após dar a quantidade de passos especificada.
 */
 
 
-// CHALLENGE
-/*
-Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
-- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
-- "print": imprime um texto de saída (output), pulando linha.
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
 
 // Para converter os valores inseridos na entrada, utilizamos a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
-*/
-
-// VARIABLES
 const posicaoInicial = parseInt(gets());
 const totalPassos = parseInt(gets());
 
-// TODO
-// Calcule a posição final do herói:
+//TODO: Calcule a posição final do herói:
 const posicaoFinal = posicaoInicial + totalPassos;
 
-// SAÍDA
 // Imprime a posição final
 print("Posicao final do heroi:", posicaoFinal);
