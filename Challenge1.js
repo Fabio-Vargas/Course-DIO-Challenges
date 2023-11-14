7618fe813483a1fbfa9285c5efeb296e9463c3f4
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-14 12:50:41 -0300

    Update Challenge1.js

--- Challenge1.js
+++ Challenge1.js
@@ -1,12 +1,36 @@
-// Variables
+// INSTRUCTIONS
+/*
+[Challenge 1] Desafio Classificador de Nível de Herói
+
+## OBJETIVO
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
+depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+- Se XP for menor que 1.000 = Bronze
+- Se XP for entre que 1.000 e 1.999 = Ferro
+- Se XP for entre que 2.000 e 3.999 = Aço
+- Se XP for entre que 4.000 e 5.999 = Prata
+- Se XP for entre que 6.000 e 7.999 = Ouro
+- Se XP for entre que 8.000 e 9.99 = Platina
+- Se XP for entre que 10.000 e 11.999 = Radiante
+- Se XP for entre que 12.000 e 13.999 = Ascendente
+- Se XP for maior ou igual a 14.000 = Imortal
+
+## SAÍDA
+Ao final deve se exibir uma mensagem: "O Herói **{nome}** está no nível **{nível}**"
+*/
+
+
+// VARIABLES
 let characterName = "HERO"
 let experience = 6514
 let rank = ""
 
-// Input
+
+// INPUT
 
 
-// Processing
+// PROCESSING
 switch (true) 
     {
         case experience < 1000:
@@ -38,5 +62,6 @@ switch (true)
             break
     }
 
-// Output
+
+// OUTPUT
 console.log("Your character called " + characterName + " is at the " + rank + " rank!")
