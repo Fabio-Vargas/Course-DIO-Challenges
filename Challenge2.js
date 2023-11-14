6aa96d0cf6ab8481903afd5db07f0cc07dc8210e
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-14 12:56:36 -0300

    Update Challenge2.js

--- Challenge2.js
+++ Challenge2.js
@@ -6,13 +6,15 @@
 Crie uma função que recebe como parâmetro a quantidade de vitórias e derrotas de um jogador,
 depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
 
-Se vitórias for menor que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100 = Lendário
-Se vitórias for maior ou igual a 101 = Imortal
+Se Vitórias for menor que 10 = Bronze
+Se Vitórias for entre 10 e 19 = Ferro
+Se Vitórias for entre 20 e 39 = Aço
+Se Vitórias for entre 40 e 59 = Prata
+Se Vitórias for entre 60 e 79 = Ouro
+Se Vitórias for entre 80 e 99 = Platina
+Se Vitórias for entre 100 e 119 = Radiante
+Se Vitórias for entre 120 e 139 = Ascendente
+Se Vitórias for maior ou igual a 140 = Imortal
 
 ## SAÍDA
 Ao final deve se exibir uma mensagem:
