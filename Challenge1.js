b6c0e1388325411494901f9d49810145ac0d0b86
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-14 12:54:07 -0300

    Update Challenge1.js

--- Challenge1.js
+++ Challenge1.js
@@ -6,18 +6,19 @@
 Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
 depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
-- Se XP for menor que 1.000 = Bronze
-- Se XP for entre que 1.000 e 1.999 = Ferro
-- Se XP for entre que 2.000 e 3.999 = Aço
-- Se XP for entre que 4.000 e 5.999 = Prata
-- Se XP for entre que 6.000 e 7.999 = Ouro
-- Se XP for entre que 8.000 e 9.99 = Platina
-- Se XP for entre que 10.000 e 11.999 = Radiante
-- Se XP for entre que 12.000 e 13.999 = Ascendente
-- Se XP for maior ou igual a 14.000 = Imortal
+Se XP for menor que 1.000 = Bronze
+Se XP for entre que 1.000 e 1.999 = Ferro
+Se XP for entre que 2.000 e 3.999 = Aço
+Se XP for entre que 4.000 e 5.999 = Prata
+Se XP for entre que 6.000 e 7.999 = Ouro
+Se XP for entre que 8.000 e 9.99 = Platina
+Se XP for entre que 10.000 e 11.999 = Radiante
+Se XP for entre que 12.000 e 13.999 = Ascendente
+Se XP for maior ou igual a 14.000 = Imortal
 
 ## SAÍDA
-Ao final deve se exibir uma mensagem: "O Herói **{nome}** está no nível **{nível}**"
+Ao final deve se exibir uma mensagem:
+"O Herói **{nome}** está no nível **{nível}**"
 */
 
 
