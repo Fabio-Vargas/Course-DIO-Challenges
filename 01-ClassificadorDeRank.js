7363147bf25a5a5e10455cbaeea54639f6af6993
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-14 15:59:53 -0300

    Update 01-ClassificadorDeRank.js

--- 01-ClassificadorDeRank.js
+++ 01-ClassificadorDeRank.js
@@ -1,24 +1,23 @@
-// INSTRUCTIONS
 /*
-[Challenge 1] Desafio Classificador de Nível de Herói
+  # [Challenge] Desafio Classificador de Nível de Herói
 
-## OBJETIVO
-Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
-depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+    ## OBJETIVO
+      Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
+      depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
-Se XP for menor que 1.000 = Bronze
-Se XP for entre 1.000 e 1.999 = Ferro
-Se XP for entre 2.000 e 3.999 = Aço
-Se XP for entre 4.000 e 5.999 = Prata
-Se XP for entre 6.000 e 7.999 = Ouro
-Se XP for entre 8.000 e 9.99 = Platina
-Se XP for entre 10.000 e 11.999 = Radiante
-Se XP for entre 12.000 e 13.999 = Ascendente
-Se XP for maior ou igual a 14.000 = Imortal
+      ### PROCESS
+        • Se XP for menor que 1.000 = Bronze
+        • Se XP for entre 1.000 e 1.999 = Ferro
+        • Se XP for entre 2.000 e 3.999 = Aço
+        • Se XP for entre 4.000 e 5.999 = Prata
+        • Se XP for entre 6.000 e 7.999 = Ouro
+        • Se XP for entre 8.000 e 9.99 = Platina
+        • Se XP for entre 10.000 e 11.999 = Radiante
+        • Se XP for entre 12.000 e 13.999 = Ascendente
+        • Se XP for maior ou igual a 14.000 = Imortal
 
-## SAÍDA
-Ao final deve se exibir uma mensagem:
-"O Herói **{nome}** está no nível **{nível}**"
+      ### OUTPUT
+        • Ao final deve se exibir uma mensagem: "O Herói **{nome}** está no nível **{nível}**"
 */
 
 
@@ -26,9 +25,6 @@ Ao final deve se exibir uma mensagem:
 let characterName = "HERO"
 let experience = 6514
 let rank = ""
-
-
-// INPUT
 
 
 // PROCESSING
