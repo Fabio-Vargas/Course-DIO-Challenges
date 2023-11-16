651fe580708b43f06f63d26da55a2edb6ebd6c9c
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-16 09:46:58 -0300

    Update 04-PontosDeExperiencia.js

--- JavaScript/04-PontosDeExperiencia.js
+++ JavaScript/04-PontosDeExperiencia.js
@@ -13,15 +13,15 @@
       Calculo do XP: Para calcular a quantidade de XP ganhos, o programa precisa considerar o nível do monstro e a dificuldade da batalha. 
       A fórmula num1 * num2 * 100 é usada para calcular essa quantidade com base nos valores fornecidos.
 
-    Explicação:
-      {num1} : Este é o nível do monstro. Quanto maior o nível do monstro, mais XP você ganhará ao derrotá-lo. 
-      Multiplicar o nível do monstro por um valor maior ajudará a refletir o aumento da recompensa de XP para monstros mais poderosos.
+        Explicação:
+          {num1} : Este é o nível do monstro. Quanto maior o nível do monstro, mais XP você ganhará ao derrotá-lo. 
+          Multiplicar o nível do monstro por um valor maior ajudará a refletir o aumento da recompensa de XP para monstros mais poderosos.
   
-      {num2} : Este é o valor da dificuldade da batalha, variando de 1 a 100. Quanto maior a dificuldade da batalha, mais XP você deve ganhar para enfrentar um desafio maior. 
-      Multiplicar pela dificuldade ajuda a ajustar a recompensa de XP com base na intensidade da batalha.
+          {num2} : Este é o valor da dificuldade da batalha, variando de 1 a 100. Quanto maior a dificuldade da batalha, mais XP você deve ganhar para enfrentar um desafio maior. 
+          Multiplicar pela dificuldade ajuda a ajustar a recompensa de XP com base na intensidade da batalha.
 
-      {100} : Este é o multiplicador constante que determina a escala geral de recompensa de XP. 
-      Multiplicar pelo nível do monstro e pela dificuldade aumenta a recompensa em 100 vezes o valor do nível e da dificuldade.
+          {100} : Este é o multiplicador constante que determina a escala geral de recompensa de XP. 
+          Multiplicar pelo nível do monstro e pela dificuldade aumenta a recompensa em 100 vezes o valor do nível e da dificuldade.
 
       ### INPUT
         • O nível do monstro (um número inteiro).
