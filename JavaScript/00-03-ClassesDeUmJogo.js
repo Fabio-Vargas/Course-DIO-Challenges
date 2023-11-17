2a9f358f42d81e81de936618c4d42ea49a8b34d7
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 17:17:27 -0300

    Update 00-03-ClassesDeUmJogo.js

--- JavaScript/00-03-ClassesDeUmJogo.js
+++ JavaScript/00-03-ClassesDeUmJogo.js
@@ -2,12 +2,12 @@
   # [Challenge] Escrevendo as Classes de um Jogo
 
     ## OBJECTIVE
-      Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+      Crie uma classe genérica que represente um herói de uma aventura e que possua as seguintes propriedades:
         • Nome
         • Idade
         • Tipo (ex: Guerreiro, Mago, Monge, Ninja )
 
-      Além disso, deve ter um método chamado "atacar" que deve atender os seguientes requisitos:
+      Além disso, deve ter um método chamado "atacar" que deve atender os seguintes requisitos:
         • Exibir a mensagem: "O {tipo} atacou usando {ataque}".
         • aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
         • e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo.
@@ -24,4 +24,25 @@
             ex: Mago atacou usando Magia
                 Guerreiro atacou usando Espada
 */
+
+
+// 
+
+class hero {
+  constructor(name, age, type, attackType) {
+    this.name = name
+    this.age = age
+    this.type = type
+    this.attackType = attackType
+  }
+
+  attack() {
+    console.log("O herói do tipo " + hero.type + " atacou usando " + hero.attackType)
+  }
+}
+
+let mage = new hero("Gandalf", 80, "Mago", "Magia")
+let warrior = new hero("Aragorn", 35, "Guerreiro", "Espada")
+let monk = new hero("Jack Chan", 30, "Monge", "Artes Marciais")
+let ninja = new hero("Naruto", 20, "Ninja", "Shuriken")
 
