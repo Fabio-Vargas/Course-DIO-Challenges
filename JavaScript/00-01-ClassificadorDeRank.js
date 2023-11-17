821f324d0bff901673879bd73d6628f0d540d03b
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-17 16:32:50 -0300

    Update 00-01-ClassificadorDeRank.js

--- JavaScript/00-01-ClassificadorDeRank.js
+++ JavaScript/00-01-ClassificadorDeRank.js
@@ -1,7 +1,7 @@
 /*
   # [Challenge] Desafio Classificador de Nível de Herói
 
-    ## OBJETIVO
+    ## OBJECTIVE
       Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, 
       depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
