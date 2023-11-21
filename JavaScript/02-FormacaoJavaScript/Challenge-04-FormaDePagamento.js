c640ec63350293f7fce74a65bfe008edf670fa3f
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-21 20:11:42 -0300

    Update Challenge-04-FormaDePagamento.js

--- JavaScript/02-FormacaoJavaScript/Challenge-04-FormaDePagamento.js
+++ JavaScript/02-FormacaoJavaScript/Challenge-04-FormaDePagamento.js
@@ -1,14 +1,14 @@
 /*
-Elabore um algoritmo que calcule o que deve ser pago por um produto,
-considerando o preço normal de etiqueta e a escolha da condição de pagamento.
-Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida 
-e efetuar o cálculo adequado.
+    Elabore um algoritmo que calcule o que deve ser pago por um produto,
+    considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+    Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida 
+    e efetuar o cálculo adequado.
 
-Código condição de pagamento:
-    • A vista no Débito, recebe 10% de desconto.
-    • A vista no Dinheiro ou PIX, recebe 15% de desconto.
-    • Em duas vezes, preço normal sem juros.
-    • Acima de duas vezes, preço normal mais juros de 10%.
+    Código condição de pagamento:
+        • A vista no Débito, recebe 10% de desconto.
+        • A vista no Dinheiro ou PIX, recebe 15% de desconto.
+        • Em duas vezes, preço normal sem juros.
+        • Acima de duas vezes, preço normal mais juros de 10%.
 */
 
 let product = "Graphics Card"
