0e034b2048443fcf0ba3a499934010fe35b0c733
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-21 20:11:27 -0300

    Update Challenge-03-CalculoIMC.js

--- JavaScript/02-FormacaoJavaScript/Challenge-03-CalculoIMC.js
+++ JavaScript/02-FormacaoJavaScript/Challenge-03-CalculoIMC.js
@@ -1,19 +1,19 @@
 /*
- O IMC - Indice de Massa Corporal é um critério da Organização Mundial da Saúde (OMS) 
- para dar uma indicação sobre a condição de peso de uma pessoa adulta.
+    O IMC - Indice de Massa Corporal é um critério da Organização Mundial da Saúde (OMS) 
+    para dar uma indicação sobre a condição de peso de uma pessoa adulta.
 
-    Fórmula do IMC:
-    IMC = peso / (altura * altura)
+        Fórmula do IMC:
+        IMC = peso / (altura * altura)
 
-Elabore um algoritmo que dado o peso e a altura de um adulto, mostre sua condição 
-de acordo com a tabela abaixo.
+    Elabore um algoritmo que dado o peso e a altura de um adulto, mostre sua condição 
+    de acordo com a tabela abaixo.
 
-    IMC em adultos:
-        • Abaixo de 18.5 = Abaixo do peso
-        • Entre 18.5 e 25 = Peso normal
-        • Entre 25 e 30 = Acima do peso
-        • Entre 30 e 40 = Obeso
-        • Acima de 40 = Obesidade Grave
+        IMC em adultos:
+            • Abaixo de 18.5 = Abaixo do peso
+            • Entre 18.5 e 25 = Peso normal
+            • Entre 25 e 30 = Acima do peso
+            • Entre 30 e 40 = Obeso
+            • Acima de 40 = Obesidade Grave
 */
 
 let pesoKG = 80
