6b1d46542c74a7144f27da2276efa82296b2ca2a
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-21 20:06:39 -0300

    Create Challenge-05-NameAgeFunction.js

---
+++ JavaScript/02-FormacaoJavaScript/Challenge-05-NameAgeFunction.js
@@ -0,0 +1,23 @@
+/*
+    Crie uma função que escreva o nome do usuário.
+    Crie uma função que verifique a idade do usuário.
+    Crie uma função "main" que chame ambas as funções anteriores.
+*/
+
+function WriteUsername(name) {
+    return "Your name is " + name
+}
+
+function VerifyAge(age) {
+    if (age < 18) {
+        return "you are a Minor."
+    } else {
+        return "you are an Adult."
+    }
+}
+
+function main(name, age) {
+    console.log(WriteUsername(name) + " and " + VerifyAge(age))
+}
+
+main("Fabio", 20)
