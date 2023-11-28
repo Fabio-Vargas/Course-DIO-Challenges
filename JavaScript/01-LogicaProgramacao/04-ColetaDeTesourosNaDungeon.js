b2e18b9a8d3e260d856b57e8967e93f231e751e5
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-28 10:15:22 -0300

    Update 04-ColetaDeTesourosNaDungeon.js

--- JavaScript/01-LogicaProgramacao/04-ColetaDeTesourosNaDungeon.js
+++ JavaScript/01-LogicaProgramacao/04-ColetaDeTesourosNaDungeon.js
@@ -1,13 +1,12 @@
 /*
   # [Challenge] Coleta de Tesouro na Dungeon
 
-    ## INSTRUCTIONS
-      Descrição
+    ## DESCRIPTION
       Sua missão é vasculhar as salas da masmorra em busca de recompensas lendárias e desafios perigosos. 
       Cada sala pode conter monstros formidáveis, tesouros preciosos ou ambos. 
       Use suas habilidades estratégicas para enfrentar as ameaças e coletar os tesouros!
 
-    ## OBJECTIVE
+    ## INSTRUCTIONS
       Escreva um programa que simule sua jornada heróica pela masmorra. 
       O programa deve percorrer cada sala e verificar se há tesouros ou monstros. 
       Se você encontrar um tesouro, colecionará a recompensa. Se encontrar um monstro, terá que derrotá-lo para continuar.
