38f189f5c7e6e671af98c0cfd0ca7e8780e38cbd
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-28 10:13:44 -0300

    Update 02-PontosDeExperiencia.js

--- JavaScript/01-LogicaProgramacao/02-PontosDeExperiencia.js
+++ JavaScript/01-LogicaProgramacao/02-PontosDeExperiencia.js
@@ -1,12 +1,12 @@
 /*
   # [Challenge] Pontos De Experiência
 
-    ## INSTRUCTIONS
+    ## DESCRIPTION
       Você é um herói em um mundo mágico repleto de monstros e desafios. 
       Sua missão agora é enfrentar inimigos e ganhar pontos de experiência (XP) para se tornar mais forte. 
       A cada vitória, você ganha XP e se aproxima de se tornar um lendário campeão.
 
-    ## OBJECTIVE
+    ## INSTRUCTIONS
       Escreva um programa simples que simule o ganho de XP após derrotar um monstro. 
       O programa deve calcular e exibir a quantidade de XP ganhos com base no nível do monstro e na dificuldade da batalha.
 
