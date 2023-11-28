d5ebea72287b4e784d2513bc09ebefb488ee4429
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-28 10:16:22 -0300

    Update 06-CombinandoNomesDePokemons.js

--- JavaScript/01-LogicaProgramacao/06-CombinandoNomesDePokemons.js
+++ JavaScript/01-LogicaProgramacao/06-CombinandoNomesDePokemons.js
@@ -1,12 +1,12 @@
 /*
   # [Challenge] Combinando nomes de Pokemons
 
-    ## INSTRUCTIONS
+    ## DESCRIPTION
       Imagine um universo onde você é um Mestre Pokémon com o poder de criar novas criaturas através da combinação de nomes. 
       Seu algoritmo é a chave para dar vida a esses seres incríveis. Você receberá uma primeira parte do nome de um Pokémon 
       e sua tarefa é completá-la com o sufixo mágico adequado, revelando o nome completo do Pokémon.
 
-    ## OBJECTIVE
+    ## INSTRUCTIONS
       Sua missão é criar uma função extraordinária chamada combinarNomePokemon. 
       Essa função possui um toque mágico que transforma uma entrada simples em algo verdadeiramente especial.
 
