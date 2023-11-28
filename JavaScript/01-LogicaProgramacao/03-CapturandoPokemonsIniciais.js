7eeda14ead9a0416f5470272fe1c31d3c7d99fa0
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-28 10:14:51 -0300

    Update 03-CapturandoPokemonsIniciais.js

--- JavaScript/01-LogicaProgramacao/03-CapturandoPokemonsIniciais.js
+++ JavaScript/01-LogicaProgramacao/03-CapturandoPokemonsIniciais.js
@@ -1,9 +1,12 @@
 /* 
   # [Challenge] Capturando Pokemons Iniciais
   
-    ## OBJECTIVE
+    ## DESCRIPTION
       No mundo dos jogos Pokémon, os treinadores começam sua jornada escolhendo um dos três Pokémons iniciais: Bulbasaur, Charmander e Mewtwo.
-      Cada treinador escolhe um dos quatro pokemons. Seu desafio é criar uma solução que permita ao jogador escolher um dos Pokémons iniciais e exibir uma mensagem de boas-vindas e o Pokémon escolhido.
+      Cada treinador escolhe um dos quatro pokemons. 
+
+    ## INSTRUCTIONS
+      Seu desafio é criar uma solução que permita ao jogador escolher um dos Pokémons iniciais e exibir uma mensagem de boas-vindas e o Pokémon escolhido.
 
       ### INPUT
         • Você receberá um número inteiro que representa a escolha do treinador: 1 para Bulbasaur, 2 para Charmander, 4 Pikachu e 5 para Mewtwo.
