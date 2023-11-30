a09a3a2287415288180854f277f2ea9c366a68c2
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-30 14:31:57 -0300

    Create Challenge-13-MaiorMenorNumeroNoConjunto.js

---
+++ JavaScript/02-FormacaoJavaScript/Challenge-13-MaiorMenorNumeroNoConjunto.js
@@ -0,0 +1,39 @@
+/*
+  Faça um programa que receba N (quantidade de numeros) e seus respectivos valores.
+  Imprima o maior numero Par e o menor numero Impar.
+
+  Exemplo:
+    Entrada: 
+      [5, 3, 4, 1, 10, 8]
+    Saída: 
+      Maior numero Par = 10
+      Menor numero Impar = 1
+*/
+
+const { gets, print } = require('./funcoes-auxiliares-ex2')
+
+const listaNumeros = gets()
+let maiorNumeroPar = null
+let menorNumeroImpar = null
+
+for (let index = 0; index < listaNumeros; index++) {
+  const numero = gets()
+  
+  if (numero % 2 === 0) {
+    if (maiorNumeroPar === null || numero > maiorNumeroPar) {
+      maiorNumeroPar = numero
+    }
+  } else {
+    if (menorNumeroImpar === 0) {
+      menorNumeroImpar = numero
+    } else if (numero < menorNumeroImpar) {
+      menorNumeroImpar = numero
+    }
+  }
+}
+
+print("Maior numero Par: " + maiorNumeroPar)
+print("Menor numero Impar: " + menorNumeroImpar
+  
+
+          
