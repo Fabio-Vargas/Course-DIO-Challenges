3d105529082a1776cf41c66c5e296d5c95ee73db
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-11-30 14:55:42 -0300

    Create Challenge-14-Salario.js

---
+++ JavaScript/02-FormacaoJavaScript/Challenge-14-Salario.js
@@ -0,0 +1,13 @@
+/*
+  Faça um programa que calcule e imprima o salario a ser transferido para um funcionario.
+  Para realizar o calculo receba o valor bruto do salario e o adicional dos beneficios.
+  O salario a ser transferido é calculado da seguinte maneira:
+
+    - Valor bruto do salario (-) percentual de imposto mediante a 
+      faixa salarial (+) adicional dos beneficios.
+
+  Para calcular o percentual de imposto segue as aliquotas:
+    - De R$ 0.00 a R$ 1100.00 = 5.00%
+    - De R$ 1100.01 a R$ 2500.00 = 10.00%
+    - Maior que R$ 2500.00 = 15.00%
+*/
