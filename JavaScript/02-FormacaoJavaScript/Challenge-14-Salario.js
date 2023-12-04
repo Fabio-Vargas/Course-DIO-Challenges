34b2626622b4eb90e4ce153a5f16e4d366edc4f9
Fábio Vargas <fabio.gp.vargas@gmail.com>
2023-12-04 13:44:34 -0300

    Update Challenge-14-Salario.js

--- JavaScript/02-FormacaoJavaScript/Challenge-14-Salario.js
+++ JavaScript/02-FormacaoJavaScript/Challenge-14-Salario.js
@@ -11,3 +11,29 @@
     - De R$ 1100.01 a R$ 2500.00 = 10.00%
     - Maior que R$ 2500.00 = 15.00%
 */
+
+const { gets, print } = require('./funcoes-auxiliares-ex3')
+
+const valorSalarioBruto = gets()
+const valorBeneficios = gets()
+
+function CalcularPorcentagem(valor, percentual) {
+  return valor * (percentual / 100)
+}
+
+function PegarAliquota(salario) {
+  if (salario >= 0 && salario <= 1100) {
+    return 5
+  } else if (salario >= 1100.01 && salario <= 2500) {
+    return 10
+  } else {
+    return 15
+  }
+}
+
+const aliquotaImposto = PegarAliquota(valorSalarioBruto)
+const valorImposto = CalcularPorcentagem(valorSalarioBruto, aliquotaImposto)
+
+const valorATransferir = valorSalarioBruto - valorImposto + valorBeneficios
+
+print(valorATransferir)
